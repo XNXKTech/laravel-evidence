2bba3f7fbfbb38b4e925c7615f93b402316e57db
ygnice <47884245+ygnice@users.noreply.github.com>
2022-03-29 18:59:41 +0800

    refactor(config/evidence): add getenv

--- config/evidence.php
+++ config/evidence.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 return [
-    'app_id' => env('EVIDENCE_APPID', 'your-app-id'),
-    'secret' => env('EVIDENCE_SECRET', 'your-app-secret'),
-    'evidence_server' => env('EVIDENCE_SERVER', 'https://smlcunzheng.tsign.cn:9443/evi-service/evidence'),
-    'esign_server' => env('ESIGN_SERVER', 'https://smlopenapi.esign.cn'),
+    'app_id' => env('EVIDENCE_APPID', 'your-app-id') ?: getenv('EVIDENCE_APPID'),
+    'secret' => env('EVIDENCE_SECRET', 'your-app-secret') ?: getenv('EVIDENCE_SECRET'),
+    'evidence_server' => env('EVIDENCE_SERVER', 'https://smlcunzheng.tsign.cn:9443/evi-service/evidence') ?: getenv('EVIDENCE_SERVER'),
+    'esign_server' => env('ESIGN_SERVER', 'https://smlopenapi.esign.cn') ?: getenv('ESIGN_SERVER'),
 ];
